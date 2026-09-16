80945ab5f2db80f3862d2f38c3facb25b75de715
Brenton Horne <brentonhorne77@gmail.com>
2026-09-16 21:10:27 +1000

    +build_pkgs.sh

---
+++ build_pkgs.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+source $HOME/.zshrc
+function empty_bdur_pkgs {
+	comm -23 \
+    <(find /var/lib/custom-packages -maxdepth 1 -type f -printf '%f\n' | sort) \
+    <(find ~/build_duration -maxdepth 1 -type f -printf '%f\n' | sort) | grep -v "libdvdread\|expect"
+}
+function upd_pkgs {
+	cat ~/logs/updates.log | grep "\[UPDATE\]" | cut -d ' ' -f 1
+}
+function upd_pkgs_err_filt {
+	cat ~/logs/updates.log |
+        grep '\[UPDATE\]' |
+        cut -d ' ' -f 1 |
+        grep -v -F -x -f <(grep -rl '^\[ERROR\]' ~/build_logs/ | cut -d '/' -f 5)
+}
+function pkg_list {
+	printf '%s\n' "$@" | uniq | sort
+}
+function build_log_pkgs {
+	while read -r pkg
+	do
+		autobuild "$pkg" -f | tee ~/build_logs/$pkg
+	done <<< $pkgs
+}
+
+pkgs=$(pkg_list "$(empty_bdur_pkgs)" "$(upd_pkgs)")
+build_log_pkgs "$pkgs"
+pkgs=$(upd_pkgs_err_filt)
+build_log_pkgs "$pkgs"
+rm_old_libs
+rm_old_share
+rm_old_docs
