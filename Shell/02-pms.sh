be582d80e731d9eb9c3d0b077a1170f85179a20a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-24 12:41:03 +1000

    Fixing autobuild-time-elapsed.sh

--- Shell/02-pms.sh
+++ Shell/02-pms.sh
@@ -434,13 +434,13 @@ function shortbd {
 function count_pkgs_left {
 	local pkg=$(ps ax | grep autobuild.sh | grep -v "grep.*autobuild.sh" | sed 's/.*sh //g' | sed 's/-f//g')
 	count=0
-for f in /var/lib/custom-packages/*(N); do
-    name=${f:t}
-    if [[ "$name" > $pkg && ! -e ~/build_duration/$name ]]; then
-        ((count++))
-    fi
-done
-echo $count
+    for f in /var/lib/custom-packages/*(N); do
+        name=${f:t}
+        if [[ "$name" > $pkg && ! -e ~/build_duration/$name ]]; then
+            ((count++))
+        fi
+    done
+    echo $count
 }
 
 function ls_pkgs_by_bdur {
--- autobuild-time-elapsed.sh
+++ autobuild-time-elapsed.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
 source $HOME/lfs-scripts/Shell/00-env.sh
 source $HOME/lfs-scripts/Shell/01-cd.sh
 source $HOME/lfs-scripts/Shell/02-pms.sh
-btimes
+echo "$(btimes)"
